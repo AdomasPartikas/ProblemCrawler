905dd3fee8a704a74265ac2cbe6ef3d121ee056c
Patrikas Adomaitis <monstras789@gmail.com>
2026-03-10 20:29:21 +0200

    Architecture skeleton (#1)
    * Added three new layers. Updated the project files accordingly.
    
    * health endpoint

--- ProblemCrawler.API/Program.cs
+++ ProblemCrawler.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/health", () => Results.Ok(new
+{
+    Status = "Healthy!",
+    Timestamp = DateTimeOffset.UtcNow
+}));
 
 app.Run();
